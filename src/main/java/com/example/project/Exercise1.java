1eaa1b4bfb2011702053c18b77082b263503b3ba
kevinPedroYare <83080715+kevinPedroYare@users.noreply.github.com>
2022-06-12 15:22:38 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -16,7 +16,41 @@ public class Exercise1 {
     public boolean esBalanceado(String s) {
         MyStack<Character> stack = new LinkedListStack<>();
         // Colocar codigo aqui
-
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            stack.push(c);
+        }
+        char c = stack.pop();
+        if(c == '(' || c == '[' || c == '{'){
+            return false;
+        }
+        for(int i = 0; i < stack.size(); i++) {
+            if(stack.isEmpty()){
+                return true;
+            }else{
+                char c2 = stack.pop();
+                if(c == '}' || c == '{'){
+                    if(c2 == '{' || c2 == ']' || c2 == ')' || c2 == '}'){
+                        c = stack.pop();
+                    }else{
+                        return false;
+                    }
+                }else if (c == ']' || c == '[') {
+                    if(c2 == '[' || c2 == ')' || c2 == '{'){
+                        c = stack.pop();
+                    }else{
+                        return false;
+                    }
+                }else{
+                    if(c2 == '(' || c2 == ')' || c2 == '[' || c2 == '}'){
+                        c = stack.pop();
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
         return false;
     }
 }
